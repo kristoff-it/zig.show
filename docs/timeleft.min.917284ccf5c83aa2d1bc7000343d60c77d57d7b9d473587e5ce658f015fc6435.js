0b1d4d9c4a4a727d16b9e8fef34c4ce876b0f75e
Loris Cro <kappaloris@gmail.com>
2023-09-02 20:35:38 +0200

    update

--- docs/timeleft.min.917284ccf5c83aa2d1bc7000343d60c77d57d7b9d473587e5ce658f015fc6435.js
+++
@@ -1,3 +0,0 @@
-function timeLeft(toDate="2020-06-07T15:00:00.000Z"){var totalSeconds=(new Date(toDate)-new Date)/1000;if(totalSeconds<(-60*60*2)){return false;}
-if(totalSeconds<0){return true;}
-totalSeconds=Math.max(totalSeconds,0);var days=Math.floor(totalSeconds/86400);totalSeconds-=days*86400;var hours=Math.floor(totalSeconds/3600)%24;totalSeconds-=hours*3600;var minutes=Math.floor(totalSeconds/60)%60;totalSeconds-=minutes*60;var seconds=Math.round(totalSeconds%60);return{days,hours,minutes,seconds};}
